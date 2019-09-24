2feb1fc43806292c3cb82a468d8efdec05e8fa49
Spencer <spencer.jackson@iconfitness.com>
2019-09-24 14:38:07 -0600

    extended to top 2500 games

--- bgg_top100.py
+++ bgg_top100.py
@@ -8,18 +8,21 @@ import copy
 page = urllib.request.urlopen("https://boardgamegeek.com/browse/boardgame")
 
 #print(page.read())
-data = page.read().decode('utf-8')
-top = [int(m.group()) for m in re.finditer('(?<=href="/boardgame/)(\d+)(?=/)',data)]
-top100 = top[0::3]
+#data = page.read().decode('utf-8')
+#top = [int(m.group()) for m in re.finditer('(?<=href="/boardgame/)(\d+)(?=/)',data)]
+#top100 = top[0::3]
 #topstring = [m.group() for m in re.finditer('(?:href="/boardgame/)(\d+)(?:/)',data)]
 #for i in range(0,10):
 #    print(top[i],topstring[3*i])
 
-top1000 = top100
+top2500 = []
 
-for p in range(2,11):
+for p in range(1,26):
     url = "https://boardgamegeek.com/browse/boardgame/page/"+str(p)
     page = urllib.request.urlopen(url)
     data = page.read().decode('utf-8')
     top = [int(m.group()) for m in re.finditer('(?<=href="/boardgame/)(\d+)(?=/)',data)]
-    top1000.extend(top[0::3])
+    top2500.extend(top[0::3])
+
+top100 = top2500[0:100]
+top1000 = top2500[0:1000]
