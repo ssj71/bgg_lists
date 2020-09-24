c4860890a08a960434231a52a1a8e96a7e80a860
ssj71 <ssjackson71@gmail.com>
2020-09-24 10:17:30 -0600

    underrated working with smoothed median filter

--- underrated.py
+++ underrated.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #spencer jackson
 
-#look at the top 10 ranked games in different weight "classes"
+#find games that could be higher ranked if they just got more ratings
 
 import bgg_get_game_stats as bggstats
 import bgg_top_ranked as bggtr
@@ -9,27 +9,45 @@ import numpy as np
 import matplotlib.pyplot as matplot
 import regression as reg
 
-top = bggtr.getTopRankedGames(5)
+top = bggtr.getTopRankedGames(100)
 stats = bggstats.getStatsSlowly(top)
+filterwidth = 46
+smoothwidth = 5 #keep this odd
 
-
-exit()
-# this method undoes the shill filter
-ratetio = stats[:,bggstats.rating_col].astype(np.float)/stats[:,bggstats.geekrating_col].astype(np.float) # get it? xD
 nvotes = stats[:,bggstats.voters_col].astype(np.int)
 l = stats.shape[0]
 
-tot  = np.array([ np.r_[:l] , ratetio.T]).T
+#this could maybe be done with broadcasting but I'd have to look it up
+medvotes = np.r_[:l-filterwidth]
+for i in range(l-filterwidth):
+    medvotes[i] = np.median(nvotes[i:i+filterwidth])
+
+size = l-filterwidth - smoothwidth +1
+smoothvotes = np.r_[:size]
+#another pass to smooth it further
+for i in range(size):
+    smoothvotes[i] = np.mean(medvotes[i:i+smoothwidth])
+
+
+irange = (np.r_[:size]+(filterwidth+smoothwidth-1)/2).astype(int)
+medratio = smoothvotes/nvotes[irange]
+
+tot  = np.array([ irange, medratio]).T
 slist = tot[tot[:,1].argsort()[::-1]]
 
-for inx in slist[:50,0]:
-    i = int(inx)
+for inx,medr in slist[:50,:]:
+    i = int(inx) #rank
     print(stats[i, bggstats.gameid_col], stats[i, bggstats.name_col])
-    print("avg rating:", stats[i, bggstats.rating_col], "\ngeek rating:", stats[i, bggstats.geekrating_col], "\nratio:",  float(stats[i, bggstats.rating_col])/float(stats[i, bggstats.geekrating_col]) )
-    print("voters:", stats[i, bggstats.voters_col], "\nyear published:", stats[i, bggstats.year_col], "\n")
+    print("rank:",i+1)
+    print("avg rating:", stats[i, bggstats.rating_col], "\ngeek rating:", stats[i, bggstats.geekrating_col])
+    print("voters:", stats[i, bggstats.voters_col])
+    print("%% of median: %.4f%% " % (100.0/medr))
+    print("year published:", stats[i, bggstats.year_col], "\n")
 
-exit()
-matplot.scatter(x, disparity)
-matplot.plot(x, y, 'k')
-
+matplot.scatter( np.r_[:l], nvotes)
+matplot.plot( np.r_[:l-filterwidth]+filterwidth/2, medvotes)
+matplot.plot( irange, smoothvotes)
+matplot.ylabel('voters')
+matplot.xlabel('rank')
+matplot.title("Voters per Rank")
 matplot.show()
