58d1f820c8a19def7d003e0c05eca1e77a3aff85
ssj71 <ssjackson71@gmail.com>
2021-06-22 01:22:41 -0600

    close to working, but issues if game title has comma

--- biggest_losers.py
+++ biggest_losers.py
@@ -5,8 +5,9 @@
 import datetime
 import numpy as np
 import csv
+import bgg_get_game_stats as bggstats
 
-latest = datetime.date(2021,6,18)
+latest = datetime.date(2021, 6, 18)
 oldest = datetime.date(2016, 10, 12)
 
 #start with the latest values (we expect it to have the complete list of games)
@@ -19,23 +20,58 @@ peaks = np.array([m[:,0].T, m[:,4].T, np.ones(l)*d.toordinal(), m[:,2], m[:,4].T
 #sort by game id
 #peaks.sort(axis=0)
 peaks = peaks[peaks[:,0].argsort(),:]
-for n in range((latest-oldest).days):
+tot = 90#(latest-oldest).days
+for n in range(tot):
+    print("\r ",100*n/tot,end="")
     d = (latest-datetime.timedelta(n))
     fn = d.strftime("%Y-%m-%d.csv")
-    m = np.genfromtxt("bgg-ranking-historicals/"+fn, delimiter=',', skip_header=1, usecols=(0,2,3,4,5,6), comments='therearenocomments')
     #we skip the string name so the cols of m are:
     #game id, year, rank, avg, geek rating, no. ratings
+    m = np.genfromtxt("bgg-ranking-historicals/"+fn, delimiter=',', skip_header=1, usecols=(0,2,3,4,5,6), comments='therearenocomments')
     idx = np.searchsorted(peaks[:,0],m[:,0])
 
     #find those that were higher before
     fallers = m[:,4] > peaks[idx,1]
-    (peaks[idx])[fallers,1] = m[fallers,4] #geek rating
-    (peaks[idx])[fallers,2] = d.toordinal() #date
-    (peaks[idx])[fallers,3] = m[fallers,2] #rank
+    ndx = idx[fallers]
+    peaks[ndx,1] = m[fallers,4] #geek rating
+    peaks[ndx,2] = d.toordinal() #date
+    peaks[ndx,3] = m[fallers,2] #rank
 
     #find those that were lower before
-    risers = m[:,4] < peaks[idx,1]
-    (peaks[idx])[risers,4] = m[risers,4] #geek rating
-    (peaks[idx])[risers,5] = d.toordinal() #date
-    (peaks[idx])[risers,6] = m[risers,2] #rank
+    #because we can't make sure this isn't before the peak, we just use the most recent as lowest
+    #risers = m[:,4] < peaks[idx,1]
+    #ndx = idx[risers]
+    #peaks[ndx,4] = m[risers,4] #geek rating
+    #peaks[ndx,5] = d.toordinal() #date
+    #peaks[ndx,6] = m[risers,2] #rank
 
+print()
+#calculate percent change in geek rating
+peaks[:,7] = 100*(peaks[:,1]-peaks[:,4])/ peaks[:,1]
+#do the same with rank
+peaks[:,8] = 100*(peaks[:,3]-peaks[:,6]) / peaks[:,3]
+#sort by fall
+sordid = peaks[peaks[:,7].argsort(),:]
+#find ones that fell rather than climbed
+fallers = sordid[:,2] < sordid[:,5]
+ids = (sordid[fallers,0])[-110:]
+stats = bggstats.getStatsSlowly(ids)
+i = 1
+for n in range(50):
+    w = np.array([])
+    while w.size == 0:
+        fell = (sordid[fallers])[-i]
+        w = np.where(stats[:,bggstats.gameid_col].astype(np.float) == fell[0])[0]
+        i += 1
+    sts = stats[w][0]
+    print()
+    print()
+    print(i-1)
+    print(sts[bggstats.gameid_col], fell[0])
+    print(n+1)
+    print(sts[bggstats.name_col], " (", sts[bggstats.year_col], ")", sep="")
+    print("peak: ", fell[1], " (rank ", int(fell[3]), ") ", datetime.date.fromordinal(int(fell[2])).strftime("%d-%b-%y"), sep="" )
+    #print("min: ", fell[4], datetime.date.fromordinal(int(fell[5])).strftime("%d-%b-%y"), "(rank ", fell[6], ")") 
+    print("current: ", fell[4]," (rank ", int(fell[6]), ")", sep="") 
+    print("change: ", fell[4]-fell[1],",", fell[7],"% (", fell[8], "% rank)")
+print("done.")
